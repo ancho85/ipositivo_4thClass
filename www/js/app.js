b4fa15ee6a2dab47ec4e01af7cba61bf9f2264d1
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-20 20:06:04 -0400

    acceso a propiedades del dispositivo mediante un listener a app.Ready

--- www/js/app.js
+++ www/js/app.js
@@ -33,6 +33,12 @@ function onAppReady() {
     if( navigator.splashscreen && navigator.splashscreen.hide ) {   // Cordova API detected
         navigator.splashscreen.hide() ;
     }
+    var element = document.getElementById('deviceProperties');
+    element.innerHTML = 'Device Model: ' + device.model + "<br/>" + 
+                        'Device Cordova: ' + device.cordova + "<br/>" + 
+                        'Device Platform: ' + device.platform + "<br/>" + 
+                        'Device UUID: ' + device.uuid + "<br/>" + 
+                        'Device Version: ' + device.version + "<br/>";
 }
 document.addEventListener("app.Ready", onAppReady, false) ;
 // document.addEventListener("deviceready", onAppReady, false) ;
