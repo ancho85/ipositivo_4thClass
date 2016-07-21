fdc0612975515301a10a67687882b69e3358ad0f
Carlos Angel Gomez Honig <ancho.corp@gmail.com>
2016-07-20 21:23:37 -0400

    geolocalización. el xdk actualizado por el IDE para utilizar la camara.

--- www/js/app.js
+++ www/js/app.js
@@ -76,3 +76,23 @@ function capturePhoto(){
 function onFail(message){
     alert("Error debido a:" + message);
 }
+
+function geolocate(){
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+function onSuccess(position){
+    var element = document.getElementById("geolocation");
+    element.innerHTML = "Latitude: " + position.coords.latitude + "<br/>" + 
+                        "Longitude:" + position.coords.longitude + "<br/>" + 
+                        "Altitude:" + position.coords.altitude + "<br/>" + 
+                        "Accuracy:" + position.coords.accuracy + "<br/>" + 
+                        "Altitude Accuracy:" + position.coords.altitudeAccuracy + "<br/>" + 
+                        "Heading" + position.coords.heading + "<br/>" + 
+                        "Speed" + position.coords.speed + "<br/>" + 
+                        "Timestamp" + position.timestamp + "<br/>";
+}
+
+function onError(error){
+    alert("code:" + error.code + "\n" + "messsage:" + error.message + "\n")
+}
